603bd4f2b2a3b4ce3d5911e7013f7a611302b35b
Joel Medeiros <jooel.medeiros@hotmail.com>
2017-07-13 21:01:36 -0300

    Basic entities

---
+++ database/migrations/2017_07_13_234604_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('im')->unique();
+            $table->string('name', 50);
+            $table->integer('category_id')->unsigned();
+            $table->boolean('free_shipping');
+            $table->text('description');
+            $table->decimal('price');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
