f003634e7b7826822ba822664b9eb8df417fa518
Joel Medeiros <jooel.medeiros@hotmail.com>
2017-07-13 21:00:02 -0300

    adding laravel collective and laravel-excel dependencies.

--- config/app.php
+++ config/app.php
@@ -167,7 +167,9 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+
 
         /*
          * Application Service Providers...
@@ -225,6 +227,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
