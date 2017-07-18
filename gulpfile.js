9699301f3e37f67215c65967ff6a60a34871c436
Joel Medeiros <jooel.medeiros@hotmail.com>
2017-07-17 22:54:47 -0300

    styles and scripts base

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const elixir = require('laravel-elixir');
 
 require('laravel-elixir-vue-2');
-
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -12,8 +11,34 @@ require('laravel-elixir-vue-2');
  | file for your application as well as publishing vendor resources.
  |
  */
+elixir(function(mix) {
 
-elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+  mix.copy(
+      'node_modules/datatables.net/js/jquery.dataTables.js',
+      'resources/assets/js/libs'
+  ).copy(
+      'node_modules/datatables.net-bs/css/dataTables.bootstrap.css',
+      'resources/assets/css/libs'
+  ).copy(
+      'node_modules/bootswatch/paper/bootstrap.min.css',
+      'resources/assets/css/libs'
+  ).copy(
+      'node_modules/bootbox/bootbox.min.js',
+      'resources/assets/js/libs'
+  );
+
+  mix.sass('app.scss')
+      .styles([
+        './resources/assets/css/custom.css',
+        './resources/assets/css/libs/dataTables.bootstrap.css',
+        './resources/assets/css/libs/bootstrap.min.css'
+      ])
+      .scripts([
+        './resources/assets/js/libs/jquery.dataTables.js',
+        './resources/assets/js/libs/bootbox.min.js'
+      ])
+      .webpack('app.js');
+
+  mix.scripts('/products.js', 'public/js/products.js');
+
 });
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,14 +7,144 @@
 
 require('./bootstrap');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+$(document).ready(function ($) {
 
-Vue.component('example', require('./components/Example.vue'));
+  $.default = {
+    /**
+     * Generates an string random
+     * @returns {string}
+     */
+    guide: function () {
+      return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    }
+  };
 
-const app = new Vue({
-    el: '#app'
+  $.alert = {
+
+    show: function(type, message, source){
+      var
+          html = [],
+          id = $.default.guide(),
+          alert = $(document).find("div#alert_content:first");
+
+      if(source) {
+        alert = $(source).hasClass('alert') ? $(source) : $(source).parents("div").find("div#alert_content:first-child");
+      }
+
+      html.push("<div id='" + id + "' class='alert " + type + " fade'>");
+      html.push("  <a class='close' data-dismiss='alert' href='#'>×</a>");
+      html.push("  <span class='alert-icon fa'></span>");
+      html.push("  <span class='alert-content'>" + message + "</span>");
+      html.push("</div>");
+      alert.html(html.join(''));
+
+      alertContent = $('#' + id);
+
+      $('html, body').animate({scrollTop: alertContent.offset().top - '100px'}, 100);
+
+      alertContent.delay(200).fadeIn("slow", function () {
+        $(this).addClass('in');
+      });
+    },
+
+    close: function (source) {
+      if (source) {
+        var alert = ($(source).hasClass('alert')) ? $(source) : $(source).parents("div").find("div#alert_content:first-child");
+        if (alert) {
+          alert.html('');
+        }
+      }
+
+      $("div.alert").not(".alert-static").remove();
+    },
+
+    formValidateResponse: function(messageGroup, source){
+      var messages = [];
+      messages.push("<ul>");
+
+      $.each(messageGroup, function(index, message){
+        messages.push('<li>' + message + '</li>');
+      });
+      messages.push("</ul>");
+
+      $.alert.show('alert-warning', messages.join(""), source);
+    }
+  };
+
+  $.modal = {
+
+    /**
+     * Returns the modal element
+     * @param id
+     * @returns {*|jQuery|HTMLElement}
+     */
+    get: function(id){
+      return $("#" + id);
+    },
+
+    load: {
+
+      /**
+       * create a new modal area
+       * @param id
+       * @param size
+       * @returns {*|jQuery|HTMLElement}
+       */
+      area: function(id, size) {
+
+        if(!id) id = $.default.guide();
+
+        return $('<div class="modal fade" tabindex="-1" role="dialog" id="' + id + '">' +
+            '<div class="modal-dialog" role="document">' +
+            '</div>' +
+            '</div>')
+      },
+
+      /**
+       * load data inside a modal
+       * @param route
+       * @param source
+       * @param id
+       * @returns string
+       */
+      show: function (route, source, id) {
+
+        if(!source) source = 'body';
+
+        $.modal.load.area(id)
+            .find('.modal-dialog')
+            .html('') // clear
+            .load(route, function () {
+              $(this).parent().modal('show');
+            })
+            .appendTo(source);
+
+        return id;
+      },
+
+      /**
+       * close an oppened modal
+       * @param id
+       */
+      close: function(id){
+        $.modal.get(id).modal('destroy');
+      }
+    }
+
+  };
+
+  $.loading = {
+
+    content: '<div class="loading-area"><div class="bg-loader"></div><div class="loader"></div></div>',
+
+    show: function(){
+      $('body').prepend($.loading.content);
+
+    },
+    close: function(){
+      $(".loading-area").fadeOut().remove()
+    }
+
+  };
+
 });
